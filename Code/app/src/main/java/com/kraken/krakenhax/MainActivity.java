8b7e79f9ac7e356019e59cc3faee8884485f45bf
mhenze27 <mhenze@ualberta.ca>
2025-10-25 10:59:46 -0600

    Add bottom navigation bar

--- Code/app/src/main/java/com/kraken/krakenhax/MainActivity.java
+++ Code/app/src/main/java/com/kraken/krakenhax/MainActivity.java
@@ -1,12 +1,15 @@
 package com.kraken.krakenhax;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        TextView tv_hello = findViewById(R.id.tv_hello);
+        BottomNavigationView bottom_navigation_bar = findViewById(R.id.bottom_navigation_bar);
+
+        bottom_navigation_bar.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.navbar_events) {
+                tv_hello.setText(R.string.events);
+            } else if (item.getItemId() == R.id.navbar_my_events) {
+                tv_hello.setText(R.string.my_events);
+            } else if (item.getItemId() == R.id.navbar_profile) {
+                tv_hello.setText(R.string.profile);
+            } else {
+                return false;
+            }
+            return true;
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
