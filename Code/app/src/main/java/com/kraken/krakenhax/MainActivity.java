af3a764619198f6b7be52125c2fec793556fbeea
mhenze27 <mhenze@ualberta.ca>
2025-10-26 11:17:04 -0600

    Set up the navbar to navigate between empty fragments for Home, Events, My Events, and Profile

--- Code/app/src/main/java/com/kraken/krakenhax/MainActivity.java
+++ Code/app/src/main/java/com/kraken/krakenhax/MainActivity.java
@@ -1,13 +1,15 @@
 package com.kraken.krakenhax;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -19,21 +21,11 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        TextView tv_hello = findViewById(R.id.tv_hello);
+        // Set up the navigation bar
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_container);
+        NavController navController = navHostFragment.getNavController();
         BottomNavigationView bottom_navigation_bar = findViewById(R.id.bottom_navigation_bar);
-
-        bottom_navigation_bar.setOnItemSelectedListener(item -> {
-            if (item.getItemId() == R.id.navbar_events) {
-                tv_hello.setText(R.string.events);
-            } else if (item.getItemId() == R.id.navbar_my_events) {
-                tv_hello.setText(R.string.my_events);
-            } else if (item.getItemId() == R.id.navbar_profile) {
-                tv_hello.setText(R.string.profile);
-            } else {
-                return false;
-            }
-            return true;
-        });
+        NavigationUI.setupWithNavController(bottom_navigation_bar, navController);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
