96b4b174a463f0d9482efc7a12a1cf16d53b0476
Carbuino <grant.william897@gmail.com>
2025-11-03 12:38:56 -0700

    Setup Comparables so Profiles can be compared

--- Code/app/src/main/java/com/kraken/krakenhax/Profile.java
+++ Code/app/src/main/java/com/kraken/krakenhax/Profile.java
@@ -1,6 +1,8 @@
 package com.kraken.krakenhax;
 import java.io.Serializable;
-public class Profile implements Serializable{
+import java.util.Objects;
+
+public class Profile implements Serializable, Comparable<Profile>{
     private String username;
     private String password;
     private String email;
@@ -43,6 +45,23 @@ public class Profile implements Serializable{
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    public int compareTo(Profile o) {
+        return this.getEmail().compareTo(o.getEmail());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Profile)) return false;
+        Profile other = (Profile) obj;
+        return Objects.equals(this.email, other.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.email);
     }
 
 }
