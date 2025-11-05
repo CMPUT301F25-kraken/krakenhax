c52ff4c6535dee270e27b423276d1e8e360b08ed
mhenze27 <mhenze@ualberta.ca>
2025-11-05 13:52:42 -0700

    Feature: Set up profile page
    - Adds a new string resource for the "My Events" page title.
    - Updates the layout for `fragment_my_events` to use the new string resource and adjusts the title style.
    - Implements the UI for the `fragment_profile` page, including a profile picture, user details (username, name, email, phone number), and buttons for editing and deleting the profile.
    - Adds placeholder data and an image for the profile page.
    - Adds internet permissions to the `AndroidManifest.xml`.
    - Defines new color and theme resources for UI styling.

--- Code/app/src/main/java/com/kraken/krakenhax/ProfileFragment.java
+++ Code/app/src/main/java/com/kraken/krakenhax/ProfileFragment.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
@@ -21,6 +25,27 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Get references to UI elements
+        ImageView profilePic = view.findViewById(R.id.profile_pic);
+        TextView username = view.findViewById(R.id.tv_username);
+        TextView name = view.findViewById(R.id.tv_name_entry);
+        TextView email = view.findViewById(R.id.tv_email_entry);
+        TextView phoneNumber = view.findViewById(R.id.tv_phone_num_entry);
+
+        // Set a default profile picture
+        profilePic.setImageResource(R.drawable.obama);
+
+        // Set default values for the info fields
+        username.setText("Obama");
+        name.setText("Barack Obama");
+        email.setText("obama@obamamail.com");
+        phoneNumber.setText("+1 (101) 1000-1110");
     }
 
 }
